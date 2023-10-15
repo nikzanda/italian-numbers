2c15d1f5a27171a57f081407099a5ad1c02c1c41
Nicolò Zandarin <nikzanda@hotmail.it>
2023-10-15 21:03:07 +0200

    publish

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'], // Build for commonJS and ESmodules
+  dts: true, // Generate declaration file (.d.ts)
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+});
